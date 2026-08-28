e328df9cafb471a9fd5f20caa616e6b7c5a09901
goatwu1993 <peteeelol@gmail.com>
2026-08-28 14:22:21 +0800

    chore: update [d ]d to use latest api

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,8 +2,8 @@ local cmp = require('cmp')
 
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = -1 }) end, opts)
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = 1 }) end, opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 vim.api.nvim_set_keymap(
     'n',
